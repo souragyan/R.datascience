53cbb4459b87ef8e21fe6b99bec3b7bce47d1720
souragyan <70279396+souragyan@users.noreply.github.com>
2020-08-26 12:22:37 -0700

    Add files via upload

---
+++ BASICS.R
@@ -0,0 +1,34 @@
+1+1
+5+4
+11%%2
+2+3*5
+exp(1)
+sqrt(10)    
+pi
+2*pi*6378
+(6*3)*20+6
+X <- 20
+y <-30
+z <-40
+
+#create a vector
+x <-c(10,20,30)
+y <-c(50,60,70)
+z<-x+y
+x*5
+x[2]
+x[2]<-33
+x
+char<-c('r','p','c',100,20)
+char
+x+(x+1)
+
+
+df<-data.frame(height=c(150,160),weight=c(65,72))
+df[1,1]
+a<-data.frame(x=1:3,y=c('a','b','c'))
+a
+a[1,1]
+a[1,]
+a[c(1,3),2]
+
